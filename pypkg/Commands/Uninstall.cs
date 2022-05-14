0554fccc6849bff30b49b681dc308b1fc8988081
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-14 15:52:53 +0300

    Ground work for an uninstall command

---
+++ pypkg/Commands/Uninstall.cs
@@ -0,0 +1,25 @@
+﻿using pypkg.Parsing;
+using pypkg.Install;
+
+namespace pypkg.Commands
+{
+    public class UninstallCommand : Command
+    {
+        public static string Name = "uninstall";
+        private static readonly Uninstallator Deinstallator = new Uninstallator();
+        
+        public async Task<CommandStatus> Execute(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: pypkg uninstall <scope>/<name>");
+                return CommandStatus.Failure;
+            }
+
+            ScopeName ParsedPackage = PackageParser.Parse(args[0]);
+
+            Deinstallator.UninstallPackage(ParsedPackage.Name);
+            return CommandStatus.Success;
+        } 
+    }
+}
---
+++ pypkg/Install/Uninstallator.cs
@@ -0,0 +1,30 @@
+﻿using pypkg;
+using pypkg.Commands;
+
+namespace pypkg.Install
+{
+    public class Uninstallator
+    {
+        public async Task<CommandStatus> UninstallPackage(string PackageName)
+        {
+            string PackagesDir = Installator.PackagesDirectory;
+            string PackagePath = PackagesDir + PackageName;
+            bool DirectoryExists = Directory.Exists(PackagePath);
+
+            if (DirectoryExists)
+            {
+                Logger.Log($"Uninstalling '{PackageName}' ");
+                Directory.Delete(PackagePath, true);
+
+                // TODO: resolve dependencies and remove them if no other package relies on it
+
+                Logger.Log($"Package '{PackageName} has been successfully removed.");
+                return CommandStatus.Success;
+            } else
+            {
+                Logger.Log($"Package '{PackageName}' is not installed and thus cannot be removed.",Logger.InfoType.Exception);
+                return CommandStatus.Failure;
+            }
+        }
+    }
+}

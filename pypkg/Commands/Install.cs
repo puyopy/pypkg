18f4ed427475405192e1805e8e6ef91ef476593c
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-22 18:18:24 +0300

    Fix arg checks in Install.cs

--- pypkg/Commands/Install.cs
+++ pypkg/Commands/Install.cs
@@ -1,6 +1,5 @@
 ﻿// pypupy - 2022
 
-using pypkg;
 using pypkg.Install;
 using Newtonsoft.Json;
 
@@ -9,14 +8,24 @@ namespace pypkg.Commands
 
     public class InstallCommand : Command
     {
-        public string name = "install";
+        //public string name = "install";
 
         public async Task<CommandStatus> Execute(string[] args)
         {
-            if (args[0] == null || args[0] == null) {
-                Console.WriteLine("Usage: pypkg install <package>");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Missing scope\n\nUsage: pypkg install <package>");
                 return CommandStatus.Success;
-            };
+            }
+            
+            string FinalNormalFolderName = null;
+
+            
+            // we have enough to read
+            if (args.Length == 2)
+            {
+                FinalNormalFolderName = args[1];
+            }
 
             string[] SplitArgs = args[0].Split("/");
 
@@ -26,8 +35,10 @@ namespace pypkg.Commands
             string Manifest = await Manifests.PullManifest(Scope,PackageName);
             if (Manifest == null)
             {
+                Logger.Log("Failed to pull manifest",Logger.InfoType.Exception);
                 return CommandStatus.Failure;   
             }
+ 
             string[] split = Manifest.Split('\n');
 
             // perform json decode magic
@@ -41,6 +52,11 @@ namespace pypkg.Commands
             Package latest = LatestVersion.package;
             Logger.Log("Installing latest");
 
+            if (FinalNormalFolderName != null)
+            {
+                PackageName = FinalNormalFolderName;
+            }
+            
             CommandStatus Result = await Installator.InstallPackage(latest,LatestVersion,PackageName);
             
             if (Result == CommandStatus.Success)
